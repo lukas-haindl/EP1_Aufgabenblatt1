2e0cb909c1bcd4a7dfaf2ea989f581d1bbd150c3
lukas-haindl <lukas.haindl03@icloud.com>
2024-10-24 12:34:22 +0200

    add to Aufgabe1, a, b, c

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -4,7 +4,28 @@
 public class Aufgabe1 {
 
     public static void main(String[] args) {
-
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        //a)
+        int sum = 0;
+        for (int i = 21; i < 420; i++) {
+            if (i % 21 == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        //b)
+        for (int i = 45; i <= 135; i++) {
+            if ((i % 5 == 0) && (i % 9 != 0)){
+                System.out.print(":" + i);
+            }
+        }
+        System.out.println(":");
+        //c)
+        for (int i = 71 - 1; i > 51; i--) {
+            if(i - 1 == 51){
+                System.out.print((char)i);
+            }
+            System.out.print("," + (char) i);
+        }
+        System.out.println();
     }
 }
