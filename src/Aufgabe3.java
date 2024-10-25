7539b5ae8695ab8c12ddc42f23e6354ec3505abe
lukas-haindl <lukas.haindl03@icloud.com>
2024-10-25 09:18:00 +0200

    implement Aufgabe3

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -9,6 +9,24 @@ public class Aufgabe3 {
 
     public static void main(String[] args) {
 
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        int maxCap = 1500;
+        int maxFoxes = 200;
+        int maxRabbits = 2500;
+        int dt = 1;
+        double numRabbits = 2500;
+        double numFoxes = 2;
+        double freeCap = maxCap - numRabbits;
+        double incRabbits, decFoxes, numContracts;
+        for (int i = 1; i <= 500; i++) {
+            System.out.format("Iteration: %d freeCap: %.2f numRabbits: %.2f numFoxes: %.2f%n", i, freeCap, numRabbits, numFoxes);
+            System.out.println("*".repeat((int)Math.round(numRabbits * 500/maxRabbits)));
+            System.out.println("#".repeat((int)Math.round(numFoxes * 500/maxFoxes)));
+            freeCap = maxCap - numRabbits;
+            incRabbits = (1.0 / maxCap) * freeCap * 0.08 * numRabbits;
+            decFoxes = 0.2 * numFoxes;
+            numContracts = numRabbits * numFoxes;
+            numRabbits = numRabbits + dt * (incRabbits - 0.002 * numContracts);
+            numFoxes = numFoxes + dt * (0.0004 * numContracts - decFoxes);
+        }
     }
 }
